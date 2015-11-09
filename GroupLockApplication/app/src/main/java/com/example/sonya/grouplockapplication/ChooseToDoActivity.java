a6cb6e98da301917b080932f59972702e1ff56f2
shparutask <sofish7184@gmail.com>
2015-11-09 22:28:06 +0300

    Add activities

---
+++ GroupLockApplication/app/src/main/java/com/example/sonya/grouplockapplication/ChooseToDoActivity.java
@@ -0,0 +1,18 @@
+package com.example.sonya.grouplockapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class ChooseToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_to_do);
+    }
+}
--- GroupLockApplication/app/src/main/java/com/example/sonya/grouplockapplication/MainActivity.java
+++ GroupLockApplication/app/src/main/java/com/example/sonya/grouplockapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sonya.grouplockapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void GoToNextActivity(View v)
+    {
+        Intent intent  = new Intent(this, ChooseToDoActivity.class);
+        startActivity(intent);
     }
 }
