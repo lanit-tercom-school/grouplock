ad0128e9bf48cc6414fb81352dbb8273d0bba796
shparutask <sofish7184@gmail.com>
2015-11-19 17:24:01 +0300

    Modified ChooseToDoActivity

--- GroupLockApplication/app/src/main/java/com/example/sonya/grouplockapplication/ChooseToDoActivity.java
+++ GroupLockApplication/app/src/main/java/com/example/sonya/grouplockapplication/ChooseToDoActivity.java
@@ -9,16 +9,57 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class ChooseToDoActivity extends AppCompatActivity {
+public class ChooseToDoActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_to_do);
+
+        spinner = (Spinner) findViewById(R.id.Main);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Menu, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
+
     public void GoToNextActivityTwo(View v)
     {
         Intent intent  = new Intent(this, LibraryActivity.class);
         startActivity(intent);
+    }
+
+    public void AskClick(View v) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Click button <Encrypt> or <Decrypt> for encrypt or decrypt file. Click <Load> for loading file from phone",
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch ( position )
+        {
+            case 1: {
+                Intent intent  = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case 2: {
+                Intent intent  = new Intent(this,LibraryActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case 3: break;
+            case 4: break;
+            default:
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
