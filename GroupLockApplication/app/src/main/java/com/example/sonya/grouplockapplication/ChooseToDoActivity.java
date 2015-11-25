b32cf8c71fe9fde63ac00c6582e1811a8336ca3c
shparutask <sofish7184@gmail.com>
2015-11-25 15:34:04 +0300

    Change ChooseToDoActivity

--- GroupLockApplication/app/src/main/java/com/example/sonya/grouplockapplication/ChooseToDoActivity.java
+++ GroupLockApplication/app/src/main/java/com/example/sonya/grouplockapplication/ChooseToDoActivity.java
@@ -3,29 +3,39 @@ package com.example.sonya.grouplockapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 
-public class ChooseToDoActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class ChooseToDoActivity extends AppCompatActivity  {
 
-    Spinner spinner;
+    public int ForEncrypt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_to_do);
-
-        spinner = (Spinner) findViewById(R.id.Main);
-
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Menu, android.R.layout.simple_spinner_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mToolbar);
     }
 
-    public void GoToNextActivityTwo(View v)
+    public void ChooseEncrypt(View v)
+    {
+        ForEncrypt = 1;
+        GoToNextActivityTwo(v);
+    }
+
+    public void ChooseDecrypt(View v)
+    {
+        ForEncrypt = 2;
+        GoToNextActivityTwo(v);
+    }
+
+    private void GoToNextActivityTwo(View v)
     {
         Intent intent  = new Intent(this, LibraryActivity.class);
         startActivity(intent);
@@ -38,28 +48,42 @@ public class ChooseToDoActivity extends AppCompatActivity implements AdapterView
         toast.show();
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        switch ( position )
-        {
-            case 1: {
-                Intent intent  = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                break;
-            }
-            case 2: {
-                Intent intent  = new Intent(this,LibraryActivity.class);
-                startActivity(intent);
-                break;
-            }
-            case 3: break;
-            case 4: break;
-            default:
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // получим идентификатор выбранного пункта меню
+        int id = item.getItemId();
 
+
+        // Операции для выбранного пункта меню
+        switch (id) {
+            case R.id.library: {
+                Intent intent = new Intent(this, LibraryActivity.class);
+                startActivity(intent);
+                return true;
+            }
+
+            case R.id.settings:
+                return true;
+
+            case R.id.info:
+                return true;
+
+            case R.id.help_button:
+            {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                    "Click button <Encrypt> or <Decrypt> for encrypt or decrypt file. Click <Load> for loading file from phone",
+                    Toast.LENGTH_SHORT);
+                toast.show();
+                return true;
+            }
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
