80d182a24ff8a6404f7c18c7b59ef8c999682e43
ivm23 <marina200964@yandex.ru>
2016-02-21 15:56:39 +0400

    Add library for choice prime numbers.

---
+++ GroupLockApplication/app/src/main/jni/PrimeNum.h
@@ -0,0 +1,42 @@
+/*
+оставить комент здесь
+*/
+#include <fstream>
+using namespace std;
+
+namespace PrimeNum{
+	static int list_primeNum[100001];
+	ifstream in("primeNumbers.in");
+
+	class prime {
+
+	public:
+		static int choice_PrimeNum(int x, int digitP);
+
+
+	private:
+		int pow(int deg);
+		void read(int n);
+		
+	};
+
+	void read(int n) {
+		for (int i = 0; i <= n; ++i) {
+			in >> list_primeNum[i];
+		}
+	}
+
+	int pow(int deg) {
+		long long res = 1;
+		for (int i = 1; i <= deg; ++i) res *= 10;
+		return res;
+	}
+
+	int choice_PrimeNum(int x, int digitX) {
+		long long a = pow(digitX - 1);
+		read(a * 10);
+		for (int i = a; i < a * 10; ++i) {
+			if (list_primeNum[i] > x) return list_primeNum[i];
+		}
+	}
+}
