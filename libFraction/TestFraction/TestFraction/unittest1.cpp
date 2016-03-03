e0507d722b441d89edbb8e0cdb87fa4327f9bbdd
ivm23 <marina200964@yandex.ru>
2016-03-03 21:45:09 +0400

    changed code style lib Fraction and modified UnitTests for lib Fraction

--- libFraction/TestFraction/TestFraction/unittest1.cpp
+++ libFraction/TestFraction/TestFraction/unittest1.cpp
@@ -1,4 +1,5 @@
-// UnitTests for lib fraction.h
+/* UnitTests for lib fraction.h
+Ivanova Mrina March 2016*/
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include  "fraction.h"
@@ -11,17 +12,21 @@ namespace TestFraction
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		Fraction a, b, o;
 		
 		TEST_METHOD(nod) {
+			Fraction a, b, o;
 			Assert::AreEqual(a.NOD(25,5), 5);
 		}
+
 		TEST_METHOD(mod) {
+			Fraction a, b, o;
 			a.c = -1;
 			a.z = 2;
 			Assert::AreEqual(a.operator%(23), 11);
 		}
+
 		TEST_METHOD(difference) {
+			Fraction a, b, o;
 			a.c = 10;
 			a.z = 3;
 			b.c = 6;
@@ -31,7 +36,9 @@ namespace TestFraction
 			Assert::AreEqual((a - b).c, o.c);
 			Assert::AreEqual((a - b).z, o.z);
 		}
+
 		TEST_METHOD(sum) {
+			Fraction a, b, o;
 			a.c = 5;
 			a.z = 2;
 			b.c = 5;
@@ -41,7 +48,9 @@ namespace TestFraction
 			Assert::AreEqual((a+b).c, o.c);
 			Assert::AreEqual((a + b).z, o.z);
 		}
+
 		TEST_METHOD(mult) {
+			Fraction a, b, o;
 			a.c = 2;
 			a.z = 3;
 			b.c = 5;
@@ -51,7 +60,9 @@ namespace TestFraction
 			Assert::AreEqual((a * b).c, o.c);
 			Assert::AreEqual((a * b).z, o.z);
 		}
+
 		TEST_METHOD(div) {
+			Fraction a, b, o;
 			a.c = 10;
 			a.z = 2;
 			b.c = 5;
@@ -61,6 +72,5 @@ namespace TestFraction
 			Assert::AreEqual((a / b).c, o.c);
 			Assert::AreEqual((a / b).z, o.z);
 		}
-
 	};
 }
--- libFraction/fraction.h
+++ libFraction/fraction.h
@@ -1,41 +1,48 @@
-#ifndef FRACTION_H
-#define FRACTION_H
-
+/*
+Class Fraction include operator+, operator-, operator*, operator/, operator% and method NOD
+Ivanova Marina March 2016
+*/
 class Fraction {
 public:
 	int c;
 	int z;
-	Fraction(){
+	Fraction() {};
 
-	};
-    Fraction(int x, int y){
+	Fraction(int x, int y) {
 		c = x;
 		z = y;
 	};
-    int NOD(int x, int y){
-		if (y == 0)
-		return x;
-		else
-		return NOD(y, x % y);
+
+	int NOD(int x, int y) {
+		if (y == 0) {
+			return x;
+		}
+		else {
+			return NOD(y, x % y);
+		}
 	};
-    int operator%(int mod){
-		if (c == mod)
+
+	int operator%(int mod) {
+		if (c == mod) {
 			return 0;
-		while (NOD(c,z) != z){
+		}
+		while (NOD(c,z) != z) {
 		c +=mod;
 		}
 		return c/z;
 	};
-	Fraction operator+(Fraction other){
+
+	Fraction operator+(Fraction other) {
 		Fraction s;
 		s.c = c*other.z+other.c*z;
 		s.z = z*other.z;
 		int nod = NOD(s.c, s.z);
 		s.c /= nod;
 		s.z /= nod;
-	return s;
+		return s;
 	};
-	Fraction operator-(Fraction other){
+
+	Fraction operator-(Fraction other) {
 		Fraction s;
 		s.c = c*other.z-other.c*z;
 		s.z = z*other.z;
@@ -44,7 +51,8 @@ public:
 		s.z /= nod;
 		return s;
 	};
-	Fraction operator*(Fraction other){
+
+	Fraction operator*(Fraction other) {
 		Fraction s;
 		s.c = c*other.c;
 		s.z = z*other.z;
@@ -53,7 +61,8 @@ public:
 		s.z /= nod;
 		return s;
 	};
-    Fraction operator/(Fraction other){
+
+	Fraction operator/(Fraction other) {
 		Fraction s;
 		s.c = c*other.z;
 		s.z = z*other.c;
@@ -63,5 +72,3 @@ public:
 		return s;
 	};
 };
-
-#endif // FRACTION_H
