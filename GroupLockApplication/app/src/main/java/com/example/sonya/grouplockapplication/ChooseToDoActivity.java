5841ab978570c8582d1e0c260f63a08326c69eb3
Sharuev Alexander <doc.juice@mail.ru>
2016-03-30 13:36:52 +0300

    Deleted testing from this branch

--- GroupLockApplication/app/src/main/java/com/example/sonya/grouplockapplication/ChooseToDoActivity.java
+++ GroupLockApplication/app/src/main/java/com/example/sonya/grouplockapplication/ChooseToDoActivity.java
@@ -5,15 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
 
 public class ChooseToDoActivity extends AppCompatActivity  {
-
-    public int ForEncrypt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,29 +19,18 @@ public class ChooseToDoActivity extends AppCompatActivity  {
         setSupportActionBar(mToolbar);
     }
 
-    public void ChooseEncrypt(View v)
-    {
-        ForEncrypt = 1;
-        GoToNextActivityTwo(v);
-    }
-
-    public void ChooseDecrypt(View v)
-    {
-        ForEncrypt = 2;
-        GoToNextActivityTwo(v);
-    }
-
-    private void GoToNextActivityTwo(View v)
+    public void chooseEncrypt(View v)
     {
         Intent intent  = new Intent(this, LibraryActivity.class);
+        intent.putExtra("state", LibraryActivity.LibraryState.ENCRYPT_SELECTING);
         startActivity(intent);
     }
 
-    public void AskClick(View v) {
-        Toast toast = Toast.makeText(getApplicationContext(),
-                "Click button <Encrypt> or <Decrypt> for encrypt or decrypt file. Click <Load> for loading file from phone",
-                Toast.LENGTH_SHORT);
-        toast.show();
+    public void chooseDecrypt(View v)
+    {
+        Intent intent  = new Intent(this, LibraryActivity.class);
+        intent.putExtra("state", LibraryActivity.LibraryState.DECRYPT_SELECTING);
+        startActivity(intent);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -68,14 +53,9 @@ public class ChooseToDoActivity extends AppCompatActivity  {
                 return true;
 
             case R.id.info:
-                return true;
-
-            case R.id.help_button:
             {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                    "Click button <Encrypt> or <Decrypt> for encrypt or decrypt file. Click <Load> for loading file from phone",
-                    Toast.LENGTH_SHORT);
-                toast.show();
+                TextView infoMessage = (TextView) findViewById(R.id.textViewInfoMessage);
+                infoMessage.setVisibility(View.VISIBLE);
                 return true;
             }
 
---
+++ GroupLockApplication/app/src/main/java/com/example/sonya/grouplockapplication/LibraryEntry.java
@@ -0,0 +1,103 @@
+package com.example.sonya.grouplockapplication;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.File;
+
+/**
+ * Entry (file or directory) in the library. Extends <code>File</code> Java type.
+ */
+public class LibraryEntry extends File implements Parcelable {
+
+    private boolean isSelected;
+    private boolean isParent;
+
+    /**
+     *
+     * @param entryFullName full name of created entry.
+     */
+    public LibraryEntry(String entryFullName) {
+        super(entryFullName);
+        isSelected = false;
+        isParent = false;
+    }
+
+    /**
+     *
+     * @param entryFullName full name of created entry.
+     * @param isParent whether this entry is parent of current directory. Used to make it possible to go back
+     *                 and its name is presented as ".." (two dots).
+     */
+    public LibraryEntry(String entryFullName, boolean isParent) {
+        this(entryFullName);
+        this.isParent = isParent;
+    }
+
+    protected LibraryEntry(Parcel in) {
+        this(in.readString());
+        boolean[] receivedBooleans = new boolean[2];
+        in.readBooleanArray(receivedBooleans);
+        isSelected = receivedBooleans[0];
+        isParent = receivedBooleans[1];
+    }
+
+    public static final Creator<LibraryEntry> CREATOR = new Creator<LibraryEntry>() {
+        @Override
+        public LibraryEntry createFromParcel(Parcel in) {
+            return new LibraryEntry(in);
+        }
+
+        @Override
+        public LibraryEntry[] newArray(int size) {
+            return new LibraryEntry[size];
+        }
+    };
+
+    /**
+     * @return <code>true</code> if this entry is a parent of directory opened by user;
+     * <code>false</code> otherwise.
+     */
+    public boolean isParent() {
+        return isParent;
+    }
+
+    /**
+     * @return <code>true</code> if this entry is selected to be encrypted or decrypted;
+     * <code>false</code> otherwise.
+     */
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    /**
+     * Sets whether this entry is selected to be encrypted or decrypted.
+     * @param isSelected state to be set.
+     */
+    public void setSelected(boolean isSelected) {
+        this.isSelected = isSelected;
+    }
+
+    /**
+     * Shows whether this entry can be selected to be encrypted or decrypted.
+     * In current implementation it depends on the extension of the file - you can select .jpg files only.
+     *
+     * @return <code>true</code> if this entry is not a directory and can be selected to be encrypted or decrypted;
+     *         <code>false</code> otherwise.
+     *
+     */
+    public boolean canBeSelected() {
+        return !isDirectory() && getName().endsWith(".jpg");
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(getAbsolutePath());
+        dest.writeBooleanArray(new boolean[]{isSelected, isParent});
+    }
+}
