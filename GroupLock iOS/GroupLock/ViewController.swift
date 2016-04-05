9e28467bf41d185a60ec36316c8e8287968f524e
Sergej Jaskiewicz <seergy_pro@me.com>
2016-04-05 20:04:39 +0300

    Blank application created
    #78

---
+++ GroupLock iOS/GroupLock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroupLock
+//
+//  Created by Sergej Jaskiewicz on 05.04.16.
+//  Copyright © 2016 Lanit-Tercom School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
