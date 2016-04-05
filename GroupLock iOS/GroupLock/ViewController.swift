4b8be3dd9caffb399e4dd993ef0ac8183f55c103
Sergej Jaskiewicz <seergy_pro@me.com>
2016-04-05 21:49:41 +0300

    Password screen set up
    #80

--- GroupLock iOS/GroupLock/ViewController.swift
+++ GroupLock iOS/GroupLock/ViewController.swift
@@ -12,14 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
-
-
+    
 }
 
