80aef67d4031b77919cb11d4c50c73ad8edc917b
Sergej Jaskiewicz <seergy_pro@me.com>
2016-04-21 20:08:32 +0300

    Applications now terminates at the end of UI tests

--- GroupLockiOS/GroupLockUITests/PasswordViewControllerTests.swift
+++ GroupLockiOS/GroupLockUITests/PasswordViewControllerTests.swift
@@ -25,6 +25,7 @@ class PasswordViewControllerTests: XCTestCase {
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        XCUIApplication().terminate()
         super.tearDown()
     }
     
