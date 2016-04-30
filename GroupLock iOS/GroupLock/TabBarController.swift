b792d07a4236a389b31f72dc3963e3ff4e45c855
Mikhail Khokhlov <gimbarrwow@gmail.com>
2016-04-30 13:30:09 +0300

    Revert "/"
    This reverts commit acc22ef876a125e0f9767be7de9f3c1d3b8780b3.

---
+++ GroupLock iOS/GroupLock/TabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarController.swift
+//  GroupLock
+//
+//  Created by Sergej Jaskiewicz on 14.04.16.
+//  Copyright © 2016 Lanit-Tercom School. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
