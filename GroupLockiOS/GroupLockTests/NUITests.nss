2dec516b60764555a67091ef319fbdf7724b4c53
Sergej Jaskiewicz <seergy_pro@me.com>
2016-05-02 15:38:05 +0300

    workaround for resolving conflicts

--- GroupLockiOS/GroupLockTests/NUITests.nss
+++
@@ -1,3 +0,0 @@
-ViewTest {
-background-color: red;
-}
--- GroupLockiOS/Pods/Target Support Files/Pods-GroupLock/Pods-GroupLock-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_GroupLockVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_GroupLockVersionString[];
-
